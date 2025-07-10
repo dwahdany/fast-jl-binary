d50d41b69e3817c36a0c7155af21f4868adbf4aa
dariush <86673488+dwahdany@users.noreply.github.com>
2025-07-10 17:57:53 +0200

    fix: change platform tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,4 @@ rm tmp/*.whl
 find tmp -name METADATA -exec sed -i   -e 's/^Author:.*/Author: Dariush Wahdany/'   -e 's/^Author-email:.*/Author-email: dariushwahdany@gmail.com/'   -e 's/^Name: fast_jl/Name: fast_jl_binary/'   {} \;
 mkdir dist
 uv run wheel pack tmp/* -d dist
+uv run wheel tags --platform-tag manylinux_2_36_x86_64 dist/*.whl --remove
