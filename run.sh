4cd58ce643ea513386899b7b8db04920a7094daf
dariush <86673488+dwahdany@users.noreply.github.com>
2025-07-10 16:39:55 +0200

    fix: use new image in build script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v $(pwd)/output:/build/output --rm fast-jl-builder uv run python -m pip wheel traker[fast] --no-build-isolation -w output
+docker run -v $(pwd)/output:/build/output --rm ghcr.io/dwahdany/fast-jl-binary:latest uv run python -m pip wheel traker[fast] --no-build-isolation -w output
