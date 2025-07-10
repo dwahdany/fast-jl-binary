176aefbd9a317689a2ebb2224339c3d1e4e8e137
dariush <86673488+dwahdany@users.noreply.github.com>
2025-07-10 20:38:11 +0200

    feat: entrypoint with python arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+python_version="${1:-3.11}"
 
-mkdir output
-uv pip install torch
-uv run python -m pip wheel traker[fast] --no-build-isolation -w output
+mkdir wheels
+uv init -p "$python_version"
+uv python pin "$python_version"
+uv add torch pip wheel
+uv run python -m pip wheel traker[fast] --no-build-isolation -w wheels
 mkdir tmp
-cp -r output/fast_jl* tmp/
+cp -r wheels/fast_jl* tmp/
 uv run wheel unpack tmp/* -d tmp
 rm tmp/*.whl
 find tmp -name METADATA -exec sed -i   -e 's/^Author:.*/Author: Dariush Wahdany/'   -e 's/^Author-email:.*/Author-email: dariushwahdany@gmail.com/'   -e 's/^Name: fast_jl/Name: fast_jl_binary/'   {} \;
@@ -32,4 +35,4 @@ cd *.dist-info
 echo "fast_jl_binary" > top_level.txt
 cd /build
 uv run wheel pack tmp/* -d dist
-uv run wheel tags --platform-tag manylinux_2_36_x86_64 dist/*.whl --remove
+uv run wheel tags --platform-tag manylinux1_x86_64 dist/*.whl --remove
