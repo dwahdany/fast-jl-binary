deef2e6c77d0723f72e00a535328c4444bb81712
dariush <86673488+dwahdany@users.noreply.github.com>
2025-07-10 20:00:27 +0200

    fix: build script renaming

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,28 @@ cp -r output/fast_jl* tmp/
 uv run wheel unpack tmp/* -d tmp
 rm tmp/*.whl
 find tmp -name METADATA -exec sed -i   -e 's/^Author:.*/Author: Dariush Wahdany/'   -e 's/^Author-email:.*/Author-email: dariushwahdany@gmail.com/'   -e 's/^Name: fast_jl/Name: fast_jl_binary/'   {} \;
-mkdir dist
+cd tmp
+for dir in fast_jl-*/; do
+  if [ -d "$dir" ]; then
+    newdir="$(echo "$dir" | sed 's/^fast_jl-/fast_jl_binary-/')"
+    mv "$dir" "$newdir"
+  fi
+done
+cd *
+for file in fast_jl.cpython-*.so; do
+  if [ -f "$file" ]; then
+    newfile="$(echo "$file" | sed 's/^fast_jl/fast_jl_binary/')"
+    mv "$file" "$newfile"
+  fi
+done
+for folder in fast_jl-*.dist-info; do
+  if [ -d "$folder" ]; then
+    newfolder="$(echo "$folder" | sed 's/^fast_jl-/fast_jl_binary-/')"
+    mv "$folder" "$newfolder"
+  fi
+done
+cd *.dist-info
+echo "fast_jl_binary" > top_level.txt
+cd /build
 uv run wheel pack tmp/* -d dist
 uv run wheel tags --platform-tag manylinux_2_36_x86_64 dist/*.whl --remove
-for oldpath in dist/fast_jl-*.whl; do
-    newpath="dist/fast_jl_binary-${oldpath#dist/fast_jl-}"
-    mv "$oldpath" "$newpath"
-done
